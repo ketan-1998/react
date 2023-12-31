d131a90e74987049b454ae45818b1ea44801386d
ketan-1998 <ketan-1998@outlook.com>
2023-12-31 15:16:07 +0530

    change color 02

--- 02-incrementa-decrement/src/App.js
+++ 02-incrementa-decrement/src/App.js
@@ -5,8 +5,8 @@ import { useState } from "react";
 function App() {
   const [value, setValue] = useState(0)
   return (
-    <div className='bg-red-600 flex justify-center items-center h-screen select-none flex-col gap-4'>
-      <div className='bg-green-600 space-x-6 px-10 py-5 flex items-center'>
+    <div className='bg-slate-600 flex justify-center items-center h-screen select-none flex-col gap-4'>
+      <div className='bg-slate-800 rounded space-x-6 px-10 py-5 flex items-center'>
         <span className="bg-slate-100 px-5 py-2 rounded-sm active:bg-slate-600" onClick={() => {
           setValue(value + 1)
         }}>
@@ -19,7 +19,7 @@ function App() {
           <FaMinus />
         </span>
       </div>
-      <div className='bg-green-600  px-10 py-3 rounded flex items-center'>
+      <div className='bg-slate-800  px-10 py-3 rounded flex items-center'>
         <buttOn className="bg-slate-300 px-10 py-2 rounded capitalize active:bg-slate-600" onClick={()=>{
           setValue(0)
         }}>reset</buttOn>
